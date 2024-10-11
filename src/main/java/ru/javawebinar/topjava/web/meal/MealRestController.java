c1581819cff34f8800c4c6be60a08063c557c7d9
atdushi <atdushi@users.noreply.github.com>
2024-10-11 17:40:04 +0300

    servlet with MealRestController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web.meal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.to.MealTo;
@@ -14,17 +15,12 @@ import java.util.List;
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
+@Controller
 public class MealRestController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private MealService service;
-
-    public List<MealTo> getAll() {
-        log.info("getAll");
-        int userId = SecurityUtil.authUserId();
-        return MealsUtil.getTos(service.getAll(userId), MealsUtil.DEFAULT_CALORIES_PER_DAY);
-    }
 
     public Meal get(int id) {
         log.info("get {}", id);
@@ -52,4 +48,9 @@ public class MealRestController {
         service.update(meal, userId);
     }
 
+    public List<MealTo> getAll() {
+        log.info("getAll");
+        int userId = SecurityUtil.authUserId();
+        return MealsUtil.getTos(service.getAll(userId), MealsUtil.DEFAULT_CALORIES_PER_DAY);
+    }
 }
