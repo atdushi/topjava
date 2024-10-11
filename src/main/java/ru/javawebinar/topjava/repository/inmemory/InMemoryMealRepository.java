ef765a3fe279c32e367803fad8bfcd7d44e4a5a9
atdushi <atdushi@users.noreply.github.com>
2024-10-11 17:27:26 +0300

    слои Meal

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,16 +1,18 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
@@ -47,7 +49,7 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
-    public Collection<Meal> getAll(int userId) {
+    public List<Meal> getAll(int userId) {
         return repository.values().stream()
                 .filter(m -> m.getUserId() == userId)
                 .sorted(new Comparator<Meal>() {
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,55 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 public class MealRestController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
     private MealService service;
+
+    public List<MealTo> getAll() {
+        log.info("getAll");
+        int userId = SecurityUtil.authUserId();
+        return MealsUtil.getTos(service.getAll(userId), MealsUtil.DEFAULT_CALORIES_PER_DAY);
+    }
+
+    public Meal get(int id) {
+        log.info("get {}", id);
+        int userId = SecurityUtil.authUserId();
+        return service.get(id, userId);
+    }
+
+    public Meal create(Meal meal) {
+        log.info("create {}", meal);
+        checkNew(meal);
+        int userId = SecurityUtil.authUserId();
+        return service.create(meal, userId);
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        int userId = SecurityUtil.authUserId();
+        service.delete(id, userId);
+    }
+
+    public void update(Meal meal, int id) {
+        log.info("update {} with id={}", meal, id);
+        assureIdConsistent(meal, id);
+        int userId = SecurityUtil.authUserId();
+        service.update(meal, userId);
+    }
 
 }
