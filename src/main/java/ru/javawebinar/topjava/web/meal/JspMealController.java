7a974387c274e444ff6300edb4dcb238acc4d885
atdushi <atdushi@users.noreply.github.com>
2024-11-10 13:27:17 +0300

    JspMealController created

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+@Controller
+public class JspMealController {
+    private static final Logger log = LoggerFactory.getLogger(JspMealController.class);
+
+    @Autowired
+    private MealService service;
+
+    @GetMapping("/meals")
+    public String getMeals(Model model) {
+        int userId = SecurityUtil.authUserId();
+        log.info("getAll for user {}", userId);
+        model.addAttribute("meals", MealsUtil.getTos(service.getAll(userId), MealsUtil.DEFAULT_CALORIES_PER_DAY));
+        return "meals";
+    }
+
+}
